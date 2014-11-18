18c50bc684a733c96a297019a9ff85f94a75921e
andela-gukpere <godson.ukpere@andela.co>
2014-11-18 14:09:37 +0100

    Few adjustments

--- scheduled.js
+++
@@ -1,6 +0,0 @@
-global._ = require('lodash');
-global.t = require('moment');
-
-var worker = require('./worker');
-console.log('sending emails to people at', t().utc().hour());
-worker.sendEmails(t().utc().hour());
